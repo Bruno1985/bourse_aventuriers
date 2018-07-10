df45335673e4f15cf8a952ccb087ba7910940321
mlallai <matthieu.lallai@gmail.com>
2018-07-10 09:39:46 +0200

    Modification controller photos

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -2,6 +2,7 @@ class AdventuresController < ApplicationController
   
    before_action :set_adventure, only: [:show, :edit, :update]
    before_action :authenticate_user!, except: [:show] 
+   before_action :require_same_user, only: [:edit, :update]
   
   def index
  
@@ -20,8 +21,19 @@ class AdventuresController < ApplicationController
         @adventure = current_user.adventures.build(room_params)
  
         if @adventure.save
+          
+          if params[:images]
  
-              redirect_to @adventure, notice:"Votre annonce a été ajouté avec succès" 
+                 params[:images].each do |i|
+ 
+                      @adventure.photos.create(image: i)
+ 
+                 end
+ 
+           end
+ 
+           @photos = @adventure.photos
+           redirect_to edit_adventure_path(@adventure), notice:"Votre aventure a été ajoutée" 
  
         else
  
@@ -31,11 +43,30 @@ class AdventuresController < ApplicationController
  
    end
    
+   def show
+   @photos = @adventure.photos
+   end
+   
+   def edit
+     @adventure = @adventure.photos
+   end
+   
    def update
  
           if @adventure.update(adventure_params)
+            if params[:images]
  
-                 redirect to @adventure, notice:"Modification enregistrée..."
+                   params[:images].each do |i|
+ 
+                        @adventure.photos.create(image: i)
+ 
+                   end
+ 
+             end
+ 
+             @photos = @adventure.photos
+ 
+            redirect to edit_adventure_path(@adventure), notice:"Modification enregistrée..."
  
           else
  
@@ -54,10 +85,22 @@ class AdventuresController < ApplicationController
  
         end
         
-        def room_params
+        def adventure_params
           
            params.require(:adventure).permit(:main_activity, :number_of_teammates, :date_of_departure, :duration, :listing_name, :summary, :address, :is_trekking, :is_trail, :is_alpinism, :is_climbing, :is_cycling, :is_alpinism, :is_motorbike, :is_sailing, :is_camping, :is_skiing, :is_fishing, :is_mountain_bike, :is_kayaking, :is_diving, :is_dog_sledding, :is_horse_riding, :is_paragliding, :price, :active)
          
          end
+         
+         def require_same_user
+ 
+                   if current_user.id != @adventure.user_id
+ 
+                        flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+ 
+                        redirect_to root_path
+ 
+                   end
+ 
+          end
  
 end
