30bde9dbe9a9e83486e5e42755be4a220e3c4b2f
mlallai <matthieu.lallai@gmail.com>
2018-07-10 17:46:28 +0200

    cloudinary ok, notifications ok

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -55,7 +55,8 @@ private
   end 
   
   def adventure_params
-    params.require(:adventure).permit(:home_type, :adventure_type, :accomodate, :bed_adventure, :bath_adventure, :listing_name, :summary, :adress, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price, :active)
+    params.require(:adventure).permit(:main_activity, :number_of_teammates, :date_of_departure, :duration, :listing_name, :summary, :address, :is_trekking, :is_trail, :is_alpinism, :is_climbing, :is_cycling, :is_alpinism, :is_motorbike, :is_sailing, :is_camping, :is_skiing, :is_fishing, :is_mountain_bike, :is_kayaking, :is_diving, :is_dog_sledding, :is_horse_riding, :is_paragliding, :price, :active)
+
   end
   
   def require_same_user
--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,17 +1,12 @@
 class Adventure < ApplicationRecord
-  belongs_to :user
-  has_many :photos
+
+belongs_to :user
+has_many :photos
   
 validates :main_activity, presence: true
-
 validates :number_of_teammates, presence: true
-
-validates :duration, presence: true
- 
-validates :listing_name, presence: true, length: {maximum: 75}
-               
-validates :summary, presence: true, length: {maximum: 600}
- 
+validates :duration, presence: true 
+validates :listing_name, presence: true, length: {maximum: 75}               
+validates :summary, presence: true, length: {maximum: 600} 
 validates :address, presence: true
-
 end
