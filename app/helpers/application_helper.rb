3f135013574049ea725a51332844954d785d2b1c
mlallai <matthieu.lallai@gmail.com>
2018-07-17 17:31:06 +0200

    connexion Facebook en place, à tester

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,18 @@
 module ApplicationHelper
   
   def avatar_url(user)
+    if user.image
+      user.image
+    else
+      if user.avatar.present?
+      user.avatar.url
     
-    if user.avatar.present?
-         user.avatar.url
-     else
-          'default_image.jpg'
-     end
+      else
+      'default_image.png'
+    end
+    
   end
   
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :path => '', 
               :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
-              :controllers => {:registrations => 'registrations' },
+              :controllers => {:registrations => 'registrations',
               :confirmations=>'confirmations',
               :omniauth_callbacks=>'omniauth_callbacks'
+            }
               
   resources :users, only: [:show]
   resources :adventures do
