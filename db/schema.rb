6e710aa5ed261fdb53df060cebd4a530081993b8
mlallai <matthieu.lallai@gmail.com>
2018-07-17 12:02:04 +0200

    création modèles Conversation et Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_143236) do
+ActiveRecord::Schema.define(version: 2018_07_17_095741) do
 
 # Could not dump table "adventures" because of following StandardError
 #   Unknown type 'stringt' for column 'listing_name'
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer "adventure_id"
