28b341ef865451c79276c5070a26a94e8f34484f
mlallai <matthieu.lallai@gmail.com>
2018-07-23 18:13:03 +0200

    on commence a faire les modifs pour la recherche

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,30 +11,15 @@ class PagesController < ApplicationController
               arrResult = Array.new
       
               if session[:bourse_aventuriers_search] && session[:bourse_aventuriers_search] != ""
-                 @adventures_address = Adventure.where(active: true).near(session[:bourse_aventuriers_search], 5, order:'distance')
+                @adventures_main_activity = Adventure.where(active: true).all
               else
-                 @adventures_address = Adventure.where(active: true).all
+                 @adventures_main_activity = Adventure.where(active: true).all
               end
       
-              @search = @adventures_address.ransack(params[:q])
+              @search = @adventures_main_activity.ransack(params[:q])
               @adventures = @search.result
       
               @arrAdventures = @adventures.to_a
       
-              if (params[:start_date] && params[:end_date] && !params[:start_date].empty? & !params[:end_date].empty?)
-                 start_date = Date.parse(params[:start_date])
-                 end_date = Date.parse(params[:end_date])
-         
-                 @adventures.each do |adventure|
-                    not_available = adventure.reservations.where("(? <= start_date AND start_date <= ?) OR 
-                    (? <= end_date AND end_date <= ?) OR (start_date < ? AND ? < end_date)", start_date, end_date,
-                    start_date, end_date, start_date, end_date).limit(1)
-            
-                    if not_available.length > 0
-                       @arrAdventures.delete(adventure)
-                    end
-                 end
-              end
-           end
 
 end
