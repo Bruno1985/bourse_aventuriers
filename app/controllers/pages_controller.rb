33706af7871a7aed6990fabb01772a2dc8686be2
mlallai <matthieu.lallai@gmail.com>
2018-09-05 18:43:47 +0200

    pagination ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,23 +4,23 @@ class PagesController < ApplicationController
     end
     
     def search
-              if params[:search].present? && params[:search].strip != ""
-                 session[:bourse_aventuriers_search] = params[:search]
-              end
+                  if params[:search].present? && params[:search].strip != ""
+                     session[:bourse_aventuriers_search] = params[:search]
+                  end
       
-              arrResult = Array.new
+                  arrResult = Array.new
       
-              if session[:bourse_aventuriers_search] && session[:bourse_aventuriers_search] != ""
-                @adventures_main_activity = Adventure.where(active: true).all
-              else
-                 @adventures_main_activity = Adventure.where(active: true).all
-              end
+                  if session[:bourse_aventuriers_search] && session[:bourse_aventuriers_search] != ""
+                    @adventures_main_activity = Adventure.where(active: true).all
+                  else
+                     @adventures_main_activity = Adventure.where(active: true).all
+                  end
       
-              @search = @adventures_main_activity.ransack(params[:q])
-              @adventures = @search.result
+                  @search = @adventures_main_activity.ransack(params[:q])
+                  @adventures = @search.result.page(params[:page]).per(2)
       
-              @arrAdventures = @adventures.to_a
-            end
+                  @arrAdventures = @adventures.to_a
+                end
       
 
 end
